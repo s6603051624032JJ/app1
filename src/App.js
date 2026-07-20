f64e052f7f85394001268ada24e0f61f3159ca6f
s6603051624032JJ <s6603051624032@email.kmutnb.ac.th>
2026-07-20 10:30:27 +0700

    Add func-conponents.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import './style.css';
+import React from "react"
+import { Content, Footer, Header } from './func-components';
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className="title">React & React Native</div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   )
 }
