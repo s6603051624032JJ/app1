ec49151b5feb713cd7080212af775eec54d6acfb
s6603051624032JJ <s6603051624032@email.kmutnb.ac.th>
2026-08-03 10:14:56 +0700

    add calculator.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import './App.css';
-import React from "react"
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
+import React from 'react'
+import { Calculator2 } from './calculator'
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+
+export default function App() {
+  return <Calculator2/>
+  
 }
-
-export default App;
