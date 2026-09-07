65c9fd575a661dd13735ca29deabd08d047869a8
s6603051624032JJ <s6603051624032@email.kmutnb.ac.th>
2026-09-07 10:35:19 +0700

    Update router4.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Router3 from './router3'
+import Router4 from './router4'
 
 export default function App() {
-  return <Router3 />
+  return <Router4 />
 }
---
+++ src/router4.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom'
+
+import './navlink.css'
+import Products from './products'
+import Home from './home'
+import Member from './member'
+
+export default function Router4() {
+  return (
+    <BrowserRouter>
+
+      <nav className="nav">
+
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "active_menu" : "menu"
+          }
+          style={({ isActive }) => {
+            return {
+              fontWeight: isActive ? "bold" : ""
+            }
+          }}
+        >
+          Home
+        </NavLink>
+
+        &nbsp;-&nbsp;
+
+        <NavLink
+          to="/products"
+          className={({ isActive }) =>
+            isActive ? "active_menu" : "menu"
+          }
+        >
+          Products
+        </NavLink>
+
+        &nbsp;-&nbsp;
+
+        <NavLink
+          to="/member"
+          className={({ isActive }) =>
+            isActive ? "active_menu" : "menu"
+          }
+        >
+          Member
+        </NavLink>
+
+        &nbsp;-&nbsp;
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "active_menu" : "menu"
+          }
+        >
+          Contact Us
+        </NavLink>
+
+      </nav>
+
+      <Routes style={{ margin: '20px' }}>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/products"
+          element={<Products />}
+        />
+
+        <Route
+          path="/member"
+          Component={Member}
+        />
+
+        <Route
+          path="/contact"
+          element={
+            <div style={{ textAlign: 'center' }}>
+              Contact Page
+            </div>
+          }
+        />
+
+        <Route
+          path="/*"
+          element={<Navigate to="/" />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
